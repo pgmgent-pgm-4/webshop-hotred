5b6bca18bf92e75a056e81175c23543eb28ec551
pgm-ryanjudd <ryanjudd@student.arteveldehs.be>
2021-05-19 15:55:30 +0200

    added 11ty support

---
+++ src/.eleventy.js
@@ -0,0 +1,17 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addPassthroughCopy({
+    'client/assets/css/main.css': 'assets/css/main.css',
+    'client/assets/images': 'assets/img',
+  });
+
+  return {
+    dir: {
+      input: 'client/views',
+      output: '../docs'
+    }
+  }
+}
